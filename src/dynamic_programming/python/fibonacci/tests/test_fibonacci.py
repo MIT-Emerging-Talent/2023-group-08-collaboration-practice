b4a5121990a3c042261c6ca93c0b726b3de3aec8
lotfullah andishmand <lotfullahandishmand@lotfullahs-MacBook-Air.local>
2024-01-18 13:31:56 +0530

    solution to fibonacci series added along with a readme file

--- src/dynamic_programming/python/fibonacci/tests/test_fibonacci.py
+++ src/dynamic_programming/python/fibonacci/tests/test_fibonacci.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-from src.fibonacci import fib_basic, fib_memo, fib_tab
+from src.fibonacci import fib_basic, fib_recursion,fib_list
 
 
 class TestFibBasic(unittest.TestCase):
@@ -35,54 +35,49 @@ class TestFibMemo(unittest.TestCase):
     def test_with_0(self):
         num = 0
         result = 0
-        self.assertEqual(fib_memo(num), result)
+        self.assertEqual(fib_recursion(num), result)
 
     def test_with_1(self):
         num = 1
         result = 1
-        self.assertEqual(fib_memo(num), result)
+        self.assertEqual(fib_recursion(num), result)
 
     def test_with_2(self):
         num = 2
         result = 1
-        self.assertEqual(fib_memo(num), result)
+        self.assertEqual(fib_recursion(num), result)
 
     def test_with_10(self):
         num = 10
         result = 55
-        self.assertEqual(fib_memo(num), result)
+        self.assertEqual(fib_recursion(num), result)
 
     def test_with_20(self):
         num = 20
         result = 6765
-        self.assertEqual(fib_memo(num), result)
+        self.assertEqual(fib_recursion(num), result)
 
 
 class TestFibTab(unittest.TestCase):
     def test_with_0(self):
         num = 0
-        result = 0
-        self.assertEqual(fib_tab(num), result)
+        result = [0,1]
+        self.assertEqual(fib_list(num), result)
 
     def test_with_1(self):
         num = 1
-        result = 1
-        self.assertEqual(fib_tab(num), result)
+        result = [0,1]
+        self.assertEqual(fib_list(num), result)
 
     def test_with_2(self):
-        num = 2
-        result = 1
-        self.assertEqual(fib_tab(num), result)
+        num = 4
+        result = [0, 1, 1, 2]
+        self.assertEqual(fib_list(num), result)
 
     def test_with_10(self):
         num = 10
-        result = 55
-        self.assertEqual(fib_tab(num), result)
-
-    def test_with_20(self):
-        num = 20
-        result = 6765
-        self.assertEqual(fib_tab(num), result)
+        result = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+        self.assertEqual(fib_list(num), result)
 
 
 if __name__ == "__main__":
