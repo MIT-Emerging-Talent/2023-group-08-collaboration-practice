5bc4fd1fa9776de5ccca81d1e2ef8499be14b9a2
Muhaddesa Noori <muhaddesanoori@gmail.com>
2024-01-21 20:58:39 +0430

    docstring and comments are added in insertion_sort.py

--- src/sorting/python/insertion_sort/src/insertion_sort.py
+++ src/sorting/python/insertion_sort/src/insertion_sort.py
@@ -1,10 +1,29 @@
 def insertionsort_v1(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+    """
+    Sorts the input array using the insertion sort algorithm.
 
-    return arr
+    Args:
+        arr (List[int]): The input array to be sorted.
+
+    Returns:
+        List[int]: The sorted array in ascending order.
+    """
+    
+    for i in range(1, len(arr)):
+        # Select the current element to be inserted at the correct position
+        key = arr[i]
+        # Initialize the index of the previous element
+        j = i - 1  
+
+        # Move elements greater than the key to the right
+        while j >= 0 and arr[j] > key:
+            # Shift the element to the right
+            arr[j + 1] = arr[j]  
+            # Move to the previous element
+            j -= 1  
+
+        # Insert the key at the correct position
+        arr[j + 1] = key  
+        
+     # Return the sorted array
+    return arr 
